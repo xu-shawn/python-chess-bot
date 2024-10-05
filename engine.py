5141dfea940173a96debce251319fc66b93cd132
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-10-06 00:48:56 +0200

    Create a benchmark command
    Bench 187811

--- engine.py
+++ engine.py
@@ -4,12 +4,15 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 import chess
+import datetime
 
+# Use for node count
+nodes: int = 0
 
 # NNUE Evaluation
 class NNUE:
     """Class to store and evaluate neural networks"""
-    def __init__(self, filename: str, feature_size: int = 768, hidden_size: int = 1024):
+    def __init__(self, filename: str, feature_size: int = 768, hidden_size: int = 512):
         """Reads in NNUE file to memory"""
         data = Path(filename).read_bytes()
         self.raw = np.frombuffer(data, dtype="<i2")
@@ -125,15 +128,15 @@ class TranspositionTable:
 
 
 class Movepick:
-    def __init__(self, ttMove: chess.Move, moves_list: chess.PseudoLegalMoveGenerator):
-        """Initalizes the MovePick object with ttMove and the board's legal move generator"""
-        self.ttMove = ttMove
+    def __init__(self, tt_move: chess.Move, moves_list: chess.PseudoLegalMoveGenerator):
+        """Initializes the MovePick object with ttMove and the board's legal move generator"""
+        self.ttMove = tt_move
         self.moves_list = moves_list
         self.stage = 0
 
     def moves(self) -> Generator:
         """Generator function that yields the next move to explore"""
-        if self.ttMove != None:
+        if self.ttMove is not None:
             yield self.ttMove
 
         for move in self.moves_list:
@@ -166,6 +169,8 @@ class Searcher:
         self, depth: int, ply: int, board: chess.Board, alpha, beta
     ) -> tuple[int, chess.Move | None]:
         """Main search, utilitzes fail-soft alpah-beta pruning inside a negamax framework"""
+        global nodes
+        nodes += 1
         if depth <= 0:
             return self.eval.full_evaluate(self.board), None
             # return self.qsearch(ply, self.board, alpha, beta), None
@@ -206,6 +211,8 @@ class Searcher:
         """Quiescient search, the idea is to analyze all captures before returning the evaluation to maintain search stability"""
         best_value: int = -self.MAX
         move_count = 0
+        global nodes
+        nodes += 1
 
         if not board.is_check():
             stand_pat: int = self.eval.full_evaluate(self.board)
@@ -239,11 +246,82 @@ class Searcher:
         return best_value
 
 
+class Benchmark:
+    def __init__(self):
+        """Initialize board and searcher"""
+        self.board = chess.Board()
+        self.searcher = Searcher()
+
+        # The bench depth
+        self.bench_depth: int = 3
+
+    def run_benchmark(self):
+        """Reset the nodes"""
+        global nodes
+        nodes = 0
+
+        test_positions = [
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+			"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 10",
+			"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 11",
+			"4rrk1/pp1n3p/3q2pQ/2p1pb2/2PP4/2P3N1/P2B2PP/4RRK1 b - - 7 19",
+			"rq3rk1/ppp2ppp/1bnpb3/3N2B1/3NP3/7P/PPPQ1PP1/2KR3R w - - 7 14",
+			"r1bq1r1k/1pp1n1pp/1p1p4/4p2Q/4Pp2/1BNP4/PPP2PPP/3R1RK1 w - - 2 14",
+			"r3r1k1/2p2ppp/p1p1bn2/8/1q2P3/2NPQN2/PPP3PP/R4RK1 b - - 2 15",
+			"r1bbk1nr/pp3p1p/2n5/1N4p1/2Np1B2/8/PPP2PPP/2KR1B1R w kq - 0 13",
+			"r1bq1rk1/ppp1nppp/4n3/3p3Q/3P4/1BP1B3/PP1N2PP/R4RK1 w - - 1 16",
+			"4r1k1/r1q2ppp/ppp2n2/4P3/5Rb1/1N1BQ3/PPP3PP/R5K1 w - - 1 17",
+			"2rqkb1r/ppp2p2/2npb1p1/1N1Nn2p/2P1PP2/8/PP2B1PP/R1BQK2R b KQ - 0 11",
+			"r1bq1r1k/b1p1npp1/p2p3p/1p6/3PP3/1B2NN2/PP3PPP/R2Q1RK1 w - - 1 16",
+			"3r1rk1/p5pp/bpp1pp2/8/q1PP1P2/b3P3/P2NQRPP/1R2B1K1 b - - 6 22",
+			"r1q2rk1/2p1bppp/2Pp4/p6b/Q1PNp3/4B3/PP1R1PPP/2K4R w - - 2 18",
+			"4k2r/1pb2ppp/1p2p3/1R1p4/3P4/2r1PN2/P4PPP/1R4K1 b - - 3 22",
+			"3q2k1/pb3p1p/4pbp1/2r5/PpN2N2/1P2P2P/5PP1/Q2R2K1 b - - 4 26",
+			"6k1/6p1/6Pp/ppp5/3pn2P/1P3K2/1PP2P2/3N4 b - - 0 1",
+            "3b4/5kp1/1p1p1p1p/pP1PpP1P/P1P1P3/3KN3/8/8 w - - 0 1",
+			"8/6pk/1p6/8/PP3p1p/5P2/4KP1q/3Q4 w - - 0 1",
+            "7k/3p2pp/4q3/8/4Q3/5Kp1/P6b/8 w - - 0 1",
+			"8/2p5/8/2kPKp1p/2p4P/2P5/3P4/8 w - - 0 1",
+            "8/1p3pp1/7p/5P1P/2k3P1/8/2K2P2/8 w - - 0 1",
+			"8/pp2r1k1/2p1p3/3pP2p/1P1P1P1P/P5KR/8/8 w - - 0 1",
+            "8/3p4/p1bk3p/Pp6/1Kp1PpPp/2P2P1P/2P5/5B2 b - - 0 1",
+			"5k2/7R/4P2p/5K2/p1r2P1p/8/8/8 b - - 0 1",
+            "6k1/6p1/P6p/r1N5/5p2/7P/1b3PP1/4R1K1 w - - 0 1",
+			"1r3k2/4q3/2Pp3b/3Bp3/2Q2p2/1p1P2P1/1P2KP2/3N4 w - - 0 1",
+			"6k1/4pp1p/3p2p1/P1pPb3/R7/1r2P1PP/3B1P2/6K1 w - - 0 1",
+            "8/3p3B/5p2/5P2/p7/PP5b/k7/6K1 w - - 0 1",
+			"5rk1/q6p/2p3bR/1pPp1rP1/1P1Pp3/P3B1Q1/1K3P2/R7 w - - 93 90",
+			"4rrk1/1p1nq3/p7/2p1P1pp/3P2bp/3Q1Bn1/PPPB4/1K2R1NR w - - 40 21",
+			"r3k2r/3nnpbp/q2pp1p1/p7/Pp1PPPP1/4BNN1/1P5P/R2Q1RK1 w kq - 0 16",
+			"3Qb1k1/1r2ppb1/pN1n2q1/Pp1Pp1Pr/4P2p/4BP2/4B1R1/1R5K b - - 11 40",
+			"4k3/3q1r2/1N2r1b1/3ppN2/2nPP3/1B1R2n1/2R1Q3/3K4 w - - 5 1"
+        ]
+
+        start_time = datetime.datetime.now()
+
+        for position in test_positions:
+            # Update the position
+            self.board.set_fen(position)
+
+            # Search the position with the bench depth
+            self.searcher.start_search(self.bench_depth, self.board)
+
+        duration = datetime.datetime.now() - start_time
+        elapsed = int(duration.total_seconds() * 1000)
+
+        # Report final stats
+        print("Time  : " + str(elapsed) + " ms")
+        print("Nodes : " + str(nodes))
+        print("NPS   : " + str(int(nodes / elapsed * 1000)))
+
+
+
 class UCI:
     def __init__(self):
         """Initialize board and searcher"""
         self.board = chess.Board()
         self.searcher = Searcher()
+        self.benchmark = Benchmark()
 
     def receive(self) -> bool:
         """Main UCI function, handles one line of command at a time"""
@@ -275,6 +353,8 @@ class UCI:
                         self.board.push(chess.Move.from_uci(move))
             case "go":
                 self.searcher.start_search(board=self.board)
+            case "bench":
+                self.benchmark.run_benchmark()
 
         return True
 
